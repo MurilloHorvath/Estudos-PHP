0da98ac3b7ac79692436781a37401f2244fdf6d4
Murillo Silveira Horvath <116307052+MurilloHorvath@users.noreply.github.com>
2022-11-23 15:23:42 -0300

    aula 3

---
+++ php/exercicios/ex002/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// tipos de dados
+
+// as variaveis em php podem ter diferentes tipos de valores
+// a mesma variavel pode ter tipos deferentes de valores
+
+$variavel = 100;
+$variavel = 'cem';
+
+// apesar de serem variaveis de tipo implicito, é importante saber:
+
+$inteiro = 100;             //  int
+$float = 10.5;              //  float (com casas decimais)
+$bool = true;               //  verdadeiro ou falso
+$string = 'ola mundo';      //  string (cadeia de caracteres)
+$array = [1,2,3];           //  array - coleção de valores
+$pessoa = new Pessoa();     // objeto com propriedades e metodos
+$nulo = null;               //  variavel com o valor nulo
